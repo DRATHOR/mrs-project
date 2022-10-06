0aee06d8a44dc17a63e24edb80631845fc319ce4
DEVENDRA RATHORE <devendra143143@gmail.com>
2022-10-06 22:35:41 +0530

    Add files via upload

---
+++ GlobleSearch.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="globle-search">
+    <!-- menu -->
+    <button class="search-button" v-on:click="handleClickEvent">
+
+      Option
+      <div class="selected-option-and-icon">
+        <span class="option-icon">O</span>
+        <span class="toggle-arrow">
+          <img src="../assets/Down-arrow.svg" alt="" />
+        </span>
+      </div>
+
+    </button>
+    <div class="select-option" v-if="showMenu">
+      <div>Open Order</div>
+      <div>Customer</div>
+      <div>Item</div>
+      <div>Part Number</div>
+      <div>Vender</div>
+      <div>Pruchase</div>
+    </div>
+    <input type="text" placeholder="Search Customer" class="search-option" />
+    <!-- input -->
+  </div>
+</template>
+  
+<script>
+// import DownArrow from '../assets/Down-arrow.svg'
+export default {
+  name: 'GlobleSearch',
+  props: {
+    msg: String
+  },
+  // components: {
+  //   DownArrow
+  // },
+  data() {
+    return {
+      showMenu: false,
+    }
+  },
+  methods: {
+    handleClickEvent() {
+      this.showMenu = !this.showMenu;
+    }
+  }
+}
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.globle-search {
+  position: relative;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  ::-webkit-input-placeholder {
+    /* Edge */
+    color: #9A9A9A;
+    font-size: 16px;
+  }
+
+  :-ms-input-placeholder {
+    /* Internet Explorer */
+    color: #9A9A9A;
+    font-size: 16px;
+  }
+
+  ::placeholder {
+    color: #9A9A9A;
+    font-size: 16px;
+  }
+
+  .search-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding:0 16px 0 24px;
+    font-weight: 600;
+    height: 40px;
+    border: none;
+    border-radius: 30px 0 0 30px;
+    background: #e8e8e8;
+    color: #000;
+    font-size: 18px;
+
+  }
+
+  .selected-option-and-icon {
+    display: flex;
+    align-items: center;
+    padding-left:8px;
+    justify-content: space-around;
+    .option-icon {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 24px;
+      height: 24px;
+      border-radius: 8px;
+      background: #5392b5;
+      color:#fff;
+    }
+    .toggle-arrow{
+      margin-left:8px;
+    }
+
+   
+  }
+  .select-option {
+      position: absolute;
+      width:145px;
+      display: flex;
+      justify-content: flex-start;
+      align-items: flex-start;
+      flex-direction: column;
+      background: #fff;
+      box-shadow: 0 0 4px gray;
+      padding: 16px;
+      top: 48px;
+      font-size: 18px;
+      color:#69697D;
+      line-height: 30px;
+      border-radius: 5px;
+    }
+  .search-option {
+    flex: 1;
+    height: 40px;
+    outline: none;
+    border: none;
+    padding: 0 16px;
+    border-radius: 0 30px 30px 0;
+    font-size: 16px;
+  }
+}
+</style>
+  
---
+++ NavigationHeader.vue
@@ -0,0 +1,159 @@
+<template>
+    <div class="navigation-header">
+        <div class="home-logo"><img src="../assets/home-logo.svg" alt="" /></div>
+        <div class="search-bar">
+            <GlobleSearch />
+        </div>
+        <div class="bill-to-info">
+            <div>
+                <p>Bill To</p>
+                <p>Not Selected</p>
+            </div>
+        </div>
+        <div class="ship-to-info">
+            <div>
+                <p>Ship To</p>
+                <p>Not Selected</p>
+            </div>
+        </div>
+        <div class="contact">
+            <div>
+                <p>Contact</p>
+                <p>Not Selected</p>
+            </div>
+        </div>
+        <div class="user-authentication-info">
+            <div class="order-info">
+                <p>Contact</p>
+                <span>Items</span>
+                <div class="close-delete-button">
+                    <button>Close</button>
+                    <button>Delete</button>
+                </div>
+            </div>
+            <div class="authentication">
+                <img src="../assets/Down-arrow.svg" alt="" />
+                <span>Invaraju</span>
+                <img src="../assets/Down-arrow.svg" alt="" />
+            </div>
+        </div>
+        <!-- logo -->
+        <!-- search bar -->
+
+        <!-- bill to -->
+        <!-- ship to -->
+        <!-- contact -->
+        <!-- order -->
+        <!-- user info{avatar,name ,login and logout} -->
+    </div>
+</template>
+  
+<script>
+import GlobleSearch from '@/components/GlobleSearch.vue';
+export default {
+    name: "NavigationHeader",
+    props: {
+        msg: String
+    },
+    components: { GlobleSearch }
+}
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.navigation-header {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    height: 113px;
+    color: #fff;
+    background-color: #3C3C3C;
+   p{
+    font-size: 16px;
+    font-weight: 600px;
+    line-height: 10px;
+   }
+    .home-logo {
+        width: 200px;
+        padding: 0 16px;
+    }
+
+    .search-bar {
+        flex: 3;
+        padding-right: 54px;
+    }
+
+    .bill-to-info {
+        flex: 1;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        border-right: 1px solid #656565;
+    }
+
+    .ship-to-info {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        flex: 1;
+        border-right: 1px solid #656565;
+        height: 100%;
+    }
+
+    .contact {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        flex: 1;
+        border-right: 1px solid #656565;
+        height: 100%;
+    }
+
+    .user-authentication-info {
+        display: flex;
+        justify-content: center;
+        flex: 2;
+        height: 100%;
+
+        .order-info {
+            flex: 1;
+            height: 100%;
+            margin-top:10px;
+            span{
+                font-size: 14px;
+                line-height: 19px;
+            }
+            .close-delete-button{
+                margin-top:-10px;
+            }
+        }
+
+        .authentication {
+            flex: 1;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            // flex-direction: column;
+        }
+    }
+}
+
+@media screen and (max-width:1400px) {
+    .navigation-header {
+        height: 80px;
+        background-color: #3C3C3C;
+    }
+}
+
+@media screen and (max-width:1200px) {
+    .navigation-header {
+        height: 80px;
+        background-color: #3C3C3C;
+    }
+}
+</style>
+  
